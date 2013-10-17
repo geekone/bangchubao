b42b2abbee4a180c59f193bca0006a7cb58ca0d0
ajaxj <ajaxj@qq.com>
2013-10-17 10:26:47 +0800

    开始添加用户的后台CRUD

--- newapp/app/controllers/admin.js
+++ newapp/app/controllers/admin.js
@@ -1,6 +1,9 @@
 //管理模块
 var mongoose = require("mongoose");
 var NewsCategory = mongoose.model("NewsCategory");
+var User = mongoose.model("User");
+var CookCategory = mongoose.model("CookCategory");
+var Cook = mongoose.model('Cook');
 
 
 exports.index = function(req,res,next){
@@ -81,20 +84,92 @@ exports.addnews = function(req,res,next){
 
 
 /**********  用户 **********/
+//所有用户
 exports.users = function(req,res,next){
-
+    User.find({},function(err,result){
+        if(err){
+            next(err);
+        }else{
+            res.render('admin/users',{layout:'admin/layout',userlist:result});
+        }
+    });
 };
 
+//添加用户
 exports.adduser = function(req,res,next){
+    var _username = req.body.username;
+    var _nickname = req.body.nickname;
+    var _password = req.body.password;
+    var _email = req.body.email;
+    var _phone = req.body.phone;
+    var _qq = req.body.qq;
+    var data = {
+        username:_username,
+        nickname:_nickname,
+        password:_password,
+        email:_email,
+        phone:_phone,
+        qq:_qq
+    };
+    var newUser = new User(data);
+    newUser.save(function(err){
+        if(err){
+            next(err);
+        }else{
+            res.redirect("/admin/users");
+        }
+    });
 
 };
 
+//删除用户
 exports.deluser = function(req,res,next){
-
+    var _uid = req.query.uid;
+    User.remove({_id:_uid},function(err){
+        if(err){
+            next(err);
+        }else{
+           res.redirect("/admin/users");
+        }
+    });
 };
 
+//更新用户
 exports.edituser = function(req,res,next){
-
+    if(req.method == "GET"){
+        var _uid = req.query.uid;
+        User.findOne({_id:_uid},function(err,result){
+            if(err){
+                next(err);
+            }else{
+               res.render("admin/edituser",{layout:"admin/layout",user:result});
+            }
+        });
+    }else if(req.method == "POST"){
+        var _uid = req.body.uid;
+        var query = {_id:_uid};
+        var _username = req.body.username;
+        var _nickname = req.body.nickname;
+        var _password = req.body.password;
+        var _email = req.body.email;
+        var _phone = req.body.phone;
+        var _qq = req.body.qq;
+        var data = {
+            username:_username,
+            nickname:_nickname,
+            password:_password,
+            email:_email,
+            phone:_phone,
+            qq:_qq
+        };
+        User.update(query,data,false,function(err){
+            if(err){
+                next(err);
+            }else{
+               res.redirect("/admin/users");
+            }
+        });
+    }
 };
 
 
